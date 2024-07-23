d8062017666398a89fbee1d7db7a8836067eb152
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-23 16:03:50 +0200

    - correzioni html+css - scrittura js del ciclo per generare i 100 quadrati

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*
 
 per creare elemento html
-    1. document.createELement --> questo consente di poterci cliccare sopra
+    1. document.createELement --> questo consente di poterci cliccare sopra in modo più facile
     2. stringa con innerHTML
 */
 
@@ -15,5 +15,22 @@ playBtn.addEventListener('click', function () {
     // genero un ciclo che mi scriva 100 elementi al clicl
     for (let i = 1; i <= 100; i++) {
         console.log(i);
+
+        // creo l'elemento in html
+        const square = document.createElement('div');
+        // metto il testo dentro il quadrato
+        square.innerHTML = i;
+        // metto il quadrato dentro il suo container
+        squareContainer.append(square);
+
+        //ascolto il click
+        square.addEventListener('click', function () {
+
+            // aggiungo la classe del quadrato selezionato
+            this.classList.add('squareSelected')
+            console.log(this.innerHTML);
+        });
+
     };
+
 })
